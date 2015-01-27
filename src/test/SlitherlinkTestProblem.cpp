ccfb4df02a07bb95d524116f37674aa45842567a
semiexp <linthya.v@gmail.com>
2015-01-28 00:38:30 +0900

    Add test problems

--- src/test/SlitherlinkTestProblem.cpp
+++ src/test/SlitherlinkTestProblem.cpp
@@ -3,7 +3,7 @@
 
 namespace Penciloid
 {
-const int PenciloidTest::NUMBER_OF_SLITHERLINK_PROBLEM = 2;
+const int PenciloidTest::NUMBER_OF_SLITHERLINK_PROBLEM = 4;
 
 void PenciloidTest::SlitherlinkLoadProblem(SlitherlinkProblem &problem, int problem_id)
 {
@@ -39,6 +39,37 @@ void PenciloidTest::SlitherlinkLoadProblem(SlitherlinkProblem &problem, int prob
 		"xxxxx010x1"
 	};
 
+	/*
+	Source: http://www.nikoli.com/ja/puzzles/slitherlink/ Sample Problem 3
+	*/
+	static const char *problem_3[] = {
+		"x3x3x31xx2",
+		"30xxx1xx3x",
+		"xx33xxx3xx",
+		"0x1x2x1xx2",
+		"xxx3x2xx23",
+		"33xx2x1xxx",
+		"2xx3x0x3x3",
+		"xx2xxx13xx",
+		"x2xx3xxx32",
+		"2xx13x3x2x"
+	};
+
+	/*
+	Source: http://www.nikoli.com/ja/puzzles/slitherlink/ Sample Problem 4
+	*/
+	static const char *problem_4[] = {
+		"x3x3xx0xxxx3xx3x1x",
+		"1xxx2x3x31x0x3xxx1",
+		"1xxx1x2xxxx1x2xxx0",
+		"x3x3xxx1xx3xxx2x2x",
+		"3x2x3xx2xx3xx3x3x1",
+		"2x3x3xx0xx3xx0x2x2",
+		"x3x1xxx2xx2xxx3x1x",
+		"2xxx0x1xxxx3x2xxx3",
+		"1xxx3x0x32x3x1xxx3",
+		"x3x1xx2xxxx2xx1x2x",
+	};
 
 	switch (problem_id)
 	{
@@ -47,6 +78,12 @@ void PenciloidTest::SlitherlinkLoadProblem(SlitherlinkProblem &problem, int prob
 		break;
 	case 2:
 		problem.Init(10, 10, (char**)problem_2);
+		break;
+	case 3:
+		problem.Init(10, 10, (char**)problem_3);
+		break;
+	case 4:
+		problem.Init(10, 18, (char**)problem_4);
 		break;
 	}
 }
