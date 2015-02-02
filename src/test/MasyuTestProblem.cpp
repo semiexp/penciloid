bd1b6ab16458532eca8cea700ed0f079e1e6a7ef
semiexp <linthya.v@gmail.com>
2015-02-03 00:03:21 +0900

    Add test problems for Masyu

--- src/test/MasyuTestProblem.cpp
+++ src/test/MasyuTestProblem.cpp
@@ -3,7 +3,7 @@
 
 namespace Penciloid
 {
-const int PenciloidTest::NUMBER_OF_MASYU_PROBLEM = 1;
+const int PenciloidTest::NUMBER_OF_MASYU_PROBLEM = 3;
 
 void PenciloidTest::MasyuLoadProblem(MasyuProblem &problem, int problem_id)
 {
@@ -23,10 +23,48 @@ void PenciloidTest::MasyuLoadProblem(MasyuProblem &problem, int problem_id)
 		"------2---",
 	};
 
+	/*
+	Source: http://www.nikoli.com/ja/puzzles/masyu/ Sample Problem 2
+	*/
+	static const char *problem_2[] = {
+		"----2-1-2-",
+		"-12-------",
+		"------1---",
+		"2------1-2",
+		"--2---2--2",
+		"----1-2---",
+		"----1-2---",
+		"-1------12",
+		"-----2----",
+		"-2--1-2---"
+	};
+
+	/*
+	Source: http://www.nikoli.com/ja/puzzles/masyu/ Sample Problem 3
+	*/
+	static const char *problem_3[] = {
+		"--2-2--22-",
+		"----------",
+		"-22---11--",
+		"----------",
+		"---2-1----",
+		"-1-----2-1",
+		"-1---2----",
+		"------2---",
+		"-2-1--222-",
+		"----------" 
+	};
+
 	switch (problem_id)
 	{
 	case 1:
 		problem.Init(10, 10, (char**)problem_1);
+		break;
+	case 2:
+		problem.Init(10, 10, (char**)problem_2);
+		break;
+	case 3:
+		problem.Init(10, 10, (char**)problem_3);
 		break;
 	}
 }
