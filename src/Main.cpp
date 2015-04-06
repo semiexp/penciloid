fce98f6d447127b3c1e591d4be83ac7b9eb77509
semiexp <linthya.v@gmail.com>
2015-04-06 17:24:51 +0900

    Add performance test switch

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,7 +3,11 @@
 
 int main()
 {
+#ifdef PL_PERFORMANCE_TEST
+	Penciloid::PenciloidTest::ConsecutivePerformanceTest("perf_test.txt");
+#else
 	Penciloid::PenciloidTest::RunTestAll();
+#endif
 
 	return 0;
 }
