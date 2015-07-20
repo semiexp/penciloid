719d7f0d5a6564f39a7ddb4b0ae088c30b831e13
semiexp <linthya.v@gmail.com>
2015-07-20 21:46:34 +0900

    Remove 'inline'

--- src/common/UnionFind.h
+++ src/common/UnionFind.h
@@ -10,7 +10,7 @@ public:
 	~UnionFind();
 
 	int Root(int p) { return parent[p] < 0 ? p : (parent[p] = Root(parent[p])); }
-	inline bool Join(int p, int q);
+	bool Join(int p, int q);
 	inline int UnionSize(int p) { return -parent[Root(p)]; }
 
 private:
