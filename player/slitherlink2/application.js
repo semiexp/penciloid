5359634392fd10cbf353698e1d1d9b3367a1e23c
semiexp <linthya.v@gmail.com>
2015-08-08 19:31:04 +0900

    Separate the initialization part to application.js

---
+++ player/slitherlink2/application.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+	var cont = new SlitherlinkController;
+	var board = new Board;
+	var field = new Field;
+	var applet = new Applet(document.getElementById("player"), cont, board);
+
+	var problems = document.getElementsByName("problem")[0].value;
+	problems = problems.split(",");
+	if (problems[problems.length - 1] == "") problem.pop();
+
+	cont.setProblems(problems);
+	cont.setBoard(board);
+	board.setField(field);
+	cont.setProblemID(0);
+}
