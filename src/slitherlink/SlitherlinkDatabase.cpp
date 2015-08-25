97efc6916e08cb4a31bd95cb05c64b072eb172c1
semiexp <linthya.v@gmail.com>
2015-08-25 23:09:18 +0900

    Apply the methods as long as new facts are revealed in SolveLocal

--- src/slitherlink/SlitherlinkDatabase.cpp
+++ src/slitherlink/SlitherlinkDatabase.cpp
@@ -97,131 +97,136 @@ int SlitherlinkDatabase::SolveLocal(int clue, int styles[12], SlitherlinkDatabas
 {
 	const int UNDECIDED = 0, LINE = 1, BLANK = 2;
 	int segments[5][5];
+	int ret = 0, ret_previous = 0;
 
 	for (int i = 0; i < 12; ++i) segments[2 + DATABASE_DY[i]][2 + DATABASE_DX[i]] = styles[i];
 
-	if (method.adjacent_lines) {
-		int n_lines =
-			(segments[2][1] == LINE ? 1 : 0) +
-			(segments[2][3] == LINE ? 1 : 0) +
-			(segments[1][2] == LINE ? 1 : 0) +
-			(segments[3][2] == LINE ? 1 : 0);
-		int n_blanks =
-			(segments[2][1] == BLANK ? 1 : 0) +
-			(segments[2][3] == BLANK ? 1 : 0) +
-			(segments[1][2] == BLANK ? 1 : 0) +
-			(segments[3][2] == BLANK ? 1 : 0);
+	int nt = 0;
+	do {
+		ret_previous = ret;
+		if (method.adjacent_lines) {
+			int n_lines =
+				(segments[2][1] == LINE ? 1 : 0) +
+				(segments[2][3] == LINE ? 1 : 0) +
+				(segments[1][2] == LINE ? 1 : 0) +
+				(segments[3][2] == LINE ? 1 : 0);
+			int n_blanks =
+				(segments[2][1] == BLANK ? 1 : 0) +
+				(segments[2][3] == BLANK ? 1 : 0) +
+				(segments[1][2] == BLANK ? 1 : 0) +
+				(segments[3][2] == BLANK ? 1 : 0);
 
-		if (n_lines > clue || 4 - n_blanks < clue) return -1;
-		
-		if (n_lines == clue) {
-			if (segments[2][1] == UNDECIDED) segments[2][1] = BLANK;
-			if (segments[2][3] == UNDECIDED) segments[2][3] = BLANK;
-			if (segments[1][2] == UNDECIDED) segments[1][2] = BLANK;
-			if (segments[3][2] == UNDECIDED) segments[3][2] = BLANK;
-		}
-		if (4 - n_blanks == clue) {
-			if (segments[2][1] == UNDECIDED) segments[2][1] = LINE;
-			if (segments[2][3] == UNDECIDED) segments[2][3] = LINE;
-			if (segments[1][2] == UNDECIDED) segments[1][2] = LINE;
-			if (segments[3][2] == UNDECIDED) segments[3][2] = LINE;
-		}
-	}
+			if (n_lines > clue || 4 - n_blanks < clue) return -1;
 
-	for (int dir = 0; dir < 4; ++dir) {
-		int dy1 = GridConstant::GRID_DY[dir], dx1 = GridConstant::GRID_DX[dir];
-		int dy2 = GridConstant::GRID_DY[(dir + 1) % 4], dx2 = GridConstant::GRID_DX[(dir + 1) % 4];
-
-		if (segments[2 + dy1 * 2 + dy2][2 + dx1 * 2 + dx2] == BLANK && segments[2 + dy1 + dy2 * 2][2 + dx1 + dx2 * 2] == BLANK) {
-			// this cell is corner
-
-			if (clue == 1 && method.corner_1) {
-				segments[2 + dy1][2 + dx1] = segments[2 + dy2][2 + dx2] = BLANK;
+			if (n_lines == clue) {
+				if (segments[2][1] == UNDECIDED) segments[2][1] = BLANK;
+				if (segments[2][3] == UNDECIDED) segments[2][3] = BLANK;
+				if (segments[1][2] == UNDECIDED) segments[1][2] = BLANK;
+				if (segments[3][2] == UNDECIDED) segments[3][2] = BLANK;
 			}
-			if (clue == 2 && method.corner_2) {
-				int out_y1, out_x1, out_y2, out_x2;
-
-				out_y1 = 2 + dy1 * 2 - dy2, out_x1 = 2 + dx1 * 2 - dx2;
-				out_y2 = 2 + dy1 - dy2 * 2, out_x2 = 2 + dx1 - dx2 * 2;
-
-				if (segments[out_y1][out_x1] == LINE) segments[out_y2][out_x2] |= BLANK;
-				if (segments[out_y1][out_x1] == BLANK) segments[out_y2][out_x2] |= LINE;
-				if (segments[out_y2][out_x2] == LINE) segments[out_y1][out_x1] |= BLANK;
-				if (segments[out_y2][out_x2] == BLANK) segments[out_y1][out_x1] |= LINE;
-
-				out_y1 = 2 - dy1 * 2 + dy2, out_x1 = 2 - dx1 * 2 + dx2;
-				out_y2 = 2 - dy1 + dy2 * 2, out_x2 = 2 - dx1 + dx2 * 2;
-
-				if (segments[out_y1][out_x1] == LINE) segments[out_y2][out_x2] |= BLANK;
-				if (segments[out_y1][out_x1] == BLANK) segments[out_y2][out_x2] |= LINE;
-				if (segments[out_y2][out_x2] == LINE) segments[out_y1][out_x1] |= BLANK;
-				if (segments[out_y2][out_x2] == BLANK) segments[out_y1][out_x1] |= LINE;
-			}
-			if (clue == 3 && method.corner_3) {
-				segments[2 + dy1][2 + dx1] = segments[2 + dy2][2 + dx2] = LINE;
+			if (4 - n_blanks == clue) {
+				if (segments[2][1] == UNDECIDED) segments[2][1] = LINE;
+				if (segments[2][3] == UNDECIDED) segments[2][3] = LINE;
+				if (segments[1][2] == UNDECIDED) segments[1][2] = LINE;
+				if (segments[3][2] == UNDECIDED) segments[3][2] = LINE;
 			}
 		}
 
-		int in_y1, in_x1, in_y2, in_x2;
-		in_y1 = 2 + dy1 * 2 + dy2, in_x1 = 2 + dx1 * 2 + dx2;
-		in_y2 = 2 + dy1 + dy2 * 2, in_x2 = 2 + dx1 + dx2 * 2;
+		for (int dir = 0; dir < 4; ++dir) {
+			int dy1 = GridConstant::GRID_DY[dir], dx1 = GridConstant::GRID_DX[dir];
+			int dy2 = GridConstant::GRID_DY[(dir + 1) % 4], dx2 = GridConstant::GRID_DX[(dir + 1) % 4];
 
-		if (clue == 3 && method.line_to_3) {
-			if (segments[in_y1][in_x1] == LINE || segments[in_y2][in_x2] == LINE) {
-				segments[2 - dy1][2 - dx1] |= LINE;
-				segments[2 - dy2][2 - dx2] |= LINE;
+			if (segments[2 + dy1 * 2 + dy2][2 + dx1 * 2 + dx2] == BLANK && segments[2 + dy1 + dy2 * 2][2 + dx1 + dx2 * 2] == BLANK) {
+				// this cell is corner
 
+				if (clue == 1 && method.corner_1) {
+					segments[2 + dy1][2 + dx1] = segments[2 + dy2][2 + dx2] = BLANK;
+				}
+				if (clue == 2 && method.corner_2) {
+					int out_y1, out_x1, out_y2, out_x2;
+
+					out_y1 = 2 + dy1 * 2 - dy2, out_x1 = 2 + dx1 * 2 - dx2;
+					out_y2 = 2 + dy1 - dy2 * 2, out_x2 = 2 + dx1 - dx2 * 2;
+
+					if (segments[out_y1][out_x1] == LINE) segments[out_y2][out_x2] |= BLANK;
+					if (segments[out_y1][out_x1] == BLANK) segments[out_y2][out_x2] |= LINE;
+					if (segments[out_y2][out_x2] == LINE) segments[out_y1][out_x1] |= BLANK;
+					if (segments[out_y2][out_x2] == BLANK) segments[out_y1][out_x1] |= LINE;
+
+					out_y1 = 2 - dy1 * 2 + dy2, out_x1 = 2 - dx1 * 2 + dx2;
+					out_y2 = 2 - dy1 + dy2 * 2, out_x2 = 2 - dx1 + dx2 * 2;
+
+					if (segments[out_y1][out_x1] == LINE) segments[out_y2][out_x2] |= BLANK;
+					if (segments[out_y1][out_x1] == BLANK) segments[out_y2][out_x2] |= LINE;
+					if (segments[out_y2][out_x2] == LINE) segments[out_y1][out_x1] |= BLANK;
+					if (segments[out_y2][out_x2] == BLANK) segments[out_y1][out_x1] |= LINE;
+				}
+				if (clue == 3 && method.corner_3) {
+					segments[2 + dy1][2 + dx1] = segments[2 + dy2][2 + dx2] = LINE;
+				}
+			}
+
+			int in_y1, in_x1, in_y2, in_x2;
+			in_y1 = 2 + dy1 * 2 + dy2, in_x1 = 2 + dx1 * 2 + dx2;
+			in_y2 = 2 + dy1 + dy2 * 2, in_x2 = 2 + dx1 + dx2 * 2;
+
+			if (clue == 3 && method.line_to_3) {
+				if (segments[in_y1][in_x1] == LINE || segments[in_y2][in_x2] == LINE) {
+					segments[2 - dy1][2 - dx1] |= LINE;
+					segments[2 - dy2][2 - dx2] |= LINE;
+
+					if (segments[in_y1][in_x1] == LINE) segments[in_y2][in_x2] |= BLANK;
+					if (segments[in_y2][in_x2] == LINE) segments[in_y1][in_x1] |= BLANK;
+				}
+			}
+
+			if (clue == 1 && method.line_to_1) {
+				if ((segments[in_y1][in_x1] == LINE && segments[in_y2][in_x2] == BLANK) ||
+					(segments[in_y1][in_x1] == BLANK && segments[in_y2][in_x2] == LINE)) {
+					segments[2 - dy1][2 - dx1] |= BLANK;
+					segments[2 - dy2][2 - dx2] |= BLANK;
+				}
+			}
+
+			if (clue == 2 && method.line_to_2) {
+				if ((segments[in_y1][in_x1] == LINE && segments[in_y2][in_x2] == BLANK) ||
+					(segments[in_y1][in_x1] == BLANK && segments[in_y2][in_x2] == LINE)) {
+					int out_y1, out_x1, out_y2, out_x2;
+
+					out_y1 = 2 - dy1, out_x1 = 2 - dx1;
+					out_y2 = 2 - dy2, out_x2 = 2 - dx2;
+
+					if (segments[out_y1][out_x1] == BLANK) segments[out_y2][out_x2] |= LINE;
+					if (segments[out_y1][out_x1] == LINE) segments[out_y2][out_x2] |= BLANK;
+					if (segments[out_y2][out_x2] == BLANK) segments[out_y1][out_x1] |= LINE;
+					if (segments[out_y2][out_x2] == LINE) segments[out_y1][out_x1] |= BLANK;
+
+					out_y1 = 2 - dy1 * 2 - dy2, out_x1 = 2 - dx1 * 2 - dx2;
+					out_y2 = 2 - dy1 - dy2 * 2, out_x2 = 2 - dx1 - dx2 * 2;
+
+					if (segments[out_y1][out_x1] == BLANK) segments[out_y2][out_x2] |= LINE;
+					if (segments[out_y1][out_x1] == LINE) segments[out_y2][out_x2] |= BLANK;
+					if (segments[out_y2][out_x2] == BLANK) segments[out_y1][out_x1] |= LINE;
+					if (segments[out_y2][out_x2] == LINE) segments[out_y1][out_x1] |= BLANK;
+				}
+			}
+
+			if ((clue == 1 && method.line_from_1 && segments[2 - dy1][2 - dx1] == BLANK && segments[2 - dy2][2 - dx2] == BLANK) ||
+				(clue == 3 && method.line_from_3 && segments[2 - dy1][2 - dx1] == LINE && segments[2 - dy2][2 - dx2] == LINE)) {
 				if (segments[in_y1][in_x1] == LINE) segments[in_y2][in_x2] |= BLANK;
+				if (segments[in_y1][in_x1] == BLANK) segments[in_y2][in_x2] |= LINE;
 				if (segments[in_y2][in_x2] == LINE) segments[in_y1][in_x1] |= BLANK;
+				if (segments[in_y2][in_x2] == BLANK) segments[in_y1][in_x1] |= LINE;
 			}
 		}
 
-		if (clue == 1 && method.line_to_1) {
-			if ((segments[in_y1][in_x1] == LINE && segments[in_y2][in_x2] == BLANK) ||
-				(segments[in_y1][in_x1] == BLANK && segments[in_y2][in_x2] == LINE)) {
-				segments[2 - dy1][2 - dx1] |= BLANK;
-				segments[2 - dy2][2 - dx2] |= BLANK;
-			}
+		ret = 0;
+		for (int i = 0; i < 12; ++i) {
+			int y = 2 + DATABASE_DY[i], x = 2 + DATABASE_DX[i];
+			if (segments[y][x] == 3) return -1;
+			ret |= segments[y][x] << (2 * i);
 		}
-
-		if (clue == 2 && method.line_to_2) {
-			if ((segments[in_y1][in_x1] == LINE && segments[in_y2][in_x2] == BLANK) ||
-				(segments[in_y1][in_x1] == BLANK && segments[in_y2][in_x2] == LINE)) {
-				int out_y1, out_x1, out_y2, out_x2;
-
-				out_y1 = 2 - dy1, out_x1 = 2 - dx1;
-				out_y2 = 2 - dy2, out_x2 = 2 - dx2;
-
-				if (segments[out_y1][out_x1] == BLANK) segments[out_y2][out_x2] |= LINE;
-				if (segments[out_y1][out_x1] == LINE) segments[out_y2][out_x2] |= BLANK;
-				if (segments[out_y2][out_x2] == BLANK) segments[out_y1][out_x1] |= LINE;
-				if (segments[out_y2][out_x2] == LINE) segments[out_y1][out_x1] |= BLANK;
-
-				out_y1 = 2 - dy1 * 2 - dy2, out_x1 = 2 - dx1 * 2 - dx2;
-				out_y2 = 2 - dy1 - dy2 * 2, out_x2 = 2 - dx1 - dx2 * 2;
-
-				if (segments[out_y1][out_x1] == BLANK) segments[out_y2][out_x2] |= LINE;
-				if (segments[out_y1][out_x1] == LINE) segments[out_y2][out_x2] |= BLANK;
-				if (segments[out_y2][out_x2] == BLANK) segments[out_y1][out_x1] |= LINE;
-				if (segments[out_y2][out_x2] == LINE) segments[out_y1][out_x1] |= BLANK;
-			}
-		}
-
-		if ((clue == 1 && method.line_from_1 && segments[2 - dy1][2 - dx1] == BLANK && segments[2 - dy2][2 - dx2] == BLANK) || 
-			(clue == 3 && method.line_from_3 && segments[2 - dy1][2 - dx1] == LINE && segments[2 - dy2][2 - dx2] == LINE)) {
-			if (segments[in_y1][in_x1] == LINE) segments[in_y2][in_x2] |= BLANK;
-			if (segments[in_y1][in_x1] == BLANK) segments[in_y2][in_x2] |= LINE;
-			if (segments[in_y2][in_x2] == LINE) segments[in_y1][in_x1] |= BLANK;
-			if (segments[in_y2][in_x2] == BLANK) segments[in_y1][in_x1] |= LINE;
-		}
-	}
-
-	int ret = 0;
-	for (int i = 0; i < 12; ++i) {
-		int y = 2 + DATABASE_DY[i], x = 2 + DATABASE_DX[i];
-		if (segments[y][x] == 3) return -1;
-		ret |= segments[y][x] << (2 * i);
-	}
+	} while (ret != ret_previous);
 
 	return ret;
 }
