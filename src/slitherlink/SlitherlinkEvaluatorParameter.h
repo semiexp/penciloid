8f18e77289c8447ae6a65f86f42fcf1125f02226
semiexp <linthya.v@gmail.com>
2015-12-31 21:17:47 +0900

    Change the parameter for the evaluator

--- src/slitherlink/SlitherlinkEvaluatorParameter.h
+++ src/slitherlink/SlitherlinkEvaluatorParameter.h
@@ -8,31 +8,31 @@ public:
 	SlitherlinkEvaluatorParameter() {
 		two_lines = 0.0;
 		avoid_cycle = 1.2;
-		hourglass_rule = 3.8;
+		hourglass_rule = 3.2;
 
-		adjacent_lines[0] = 0.7;
-		adjacent_lines[1] = 1.5;
+		adjacent_lines[0] = 1.0;
+		adjacent_lines[1] = 1.4;
 		adjacent_lines[2] = 1.2;
 		adjacent_lines[3] = 1.2;
-		adjacent_3 = 2.9;
-		diagonal_3 = 2.3;
+		adjacent_3 = 2.7;
+		diagonal_3 = 2.0;
 		corner_clue[0] = 0.0;
-		corner_clue[1] = 2.7;
-		corner_clue[2] = 2.6;
-		corner_clue[3] = 1.6;
+		corner_clue[1] = 3.2;
+		corner_clue[2] = 3.3;
+		corner_clue[3] = 1.4;
 		line_to_clue[0] = 0.0;
-		line_to_clue[1] = 2.6;
+		line_to_clue[1] = 2.5;
 		line_to_clue[2] = 1.2;
-		line_to_clue[3] = 1.5;
+		line_to_clue[3] = 1.3;
 		line_from_clue[0] = 0.0;
-		line_from_clue[1] = 4.3;
+		line_from_clue[1] = 4.5;
 		line_from_clue[2] = 0.0;
-		line_from_clue[3] = 4.0;
-		diagonal_chain = 2.7;
-		inout_rule = 3.6;
+		line_from_clue[3] = 4.4;
+		diagonal_chain = 3.1;
+		inout_rule = 4.4;
 
-		locality_base = 2.8;
-		alternative_dimension = 2.4;
+		locality_base = 2.4;
+		alternative_dimension = 3.0;
 	}
 
 	double two_lines;
